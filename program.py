b1f9e12bab8c4b746d551f2b04a58c59d144f27f
philipandag <philipgoclever@gmail.com>
2023-03-17 20:43:33 +0100

    zmienilem tylko nprobs na potege 2 bo byla pomylka

--- program.py
+++ program.py
@@ -12,7 +12,7 @@ sample_rate, samples = wavfile.read('piano.wav')
 #usrednione probki fali - jeden kanal
 samples = np.mean(np.array(samples), axis=1) 
 
-frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate, nperseg=8162)
+frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate, nperseg=8192)
 
 mid = np.mean(spectrogram)
 max = np.max(spectrogram)
@@ -23,7 +23,7 @@ sum_duplicates(notes)
 scale_volumes_to_percent(notes, max_value(notes)) # wazne by skalowac po sumowaniu inaczej sumy moga wyjsc poza zakres
 sort_by_intensity(notes)
 
-print_notes_list(notes, times)
+#print_notes_list(notes, times)
 #print_notes_list(notes, times)
 
 #plot_spectrogram(frequencies, times, spectrogram)
