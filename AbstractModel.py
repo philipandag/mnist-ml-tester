163fe5b428b11055847813ddecb4ecc2a6a38b05
Filip Gołaś <philipgoclever@gmail.com>
2023-05-10 22:16:52 +0200

    Doprecyzowano abstrakcyjną klasę model by lepiej informowała o tym jak ma wyglądać jej implementacja

--- AbstractModel.py
+++ AbstractModel.py
@@ -1,13 +1,31 @@
+import numpy as np
+
 class Model(object):
 
+    # initialize the object
     def __init__(self):
         raise NotImplementedError("__init__ not implemented")
+    
+    # return a list of names of hiperparameters of the model
+    def get_hiperparameters(self) -> list:
+        raise NotImplementedError("get_hiperparameters not implemented")
+    
+    # set the hiperparameters of the model to the values given as a dict of {"name": value}
+    def set_hiperparameters(self, hiperparameters: dict):
+        raise NotImplementedError("set_hiperparameters not implemented")
+    
+    # return a description of the hiperparameter with the given name (type / possible values / what will it affect / etc.)
+    def get_hiperparameter_description(self, name: str) -> str:
+        raise NotImplementedError("get_hiperparameter_description not implemented")
 
-    def fit(self, X_train, y_train):
+    # fit the model to the training data
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray) -> None:
         raise NotImplementedError("fit not implemented")
 
-    def predict(self, X_test):
+    # return an array of 10 floats where each float represents the probability of the corresponding digit
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
         raise NotImplementedError("predict not implemented")
 
-    def score(self, X_test, y_test):
+    # return the mean accuracy on the given test data and labels
+    def score(self, X_test: np.ndarray, y_test: np.ndarray) -> float:
         raise NotImplementedError("score not implemented")
